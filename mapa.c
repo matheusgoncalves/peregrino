8cfd033fa827b798cbc439c5496c02b10c2b4a0d
Matheus <msgoncalves@inf.ufpel.edu.br>
2025-03-17 21:19:01 -0300

    Modificando e removendo o recurso de decisões da estrutura da árvore de mapas

--- mapa.c
+++ mapa.c
@@ -4,16 +4,13 @@
 #include "mapa.h"
 
 // Função para criar um novo local no mapa
-NoMapa* criarNoMapa(char nome[], char descricao[], int tem_decisao) {
+NoMapa* criarNoMapa(char nome[], char descricao[]) {
     NoMapa *novo = (NoMapa*) malloc(sizeof(NoMapa));
 
     strcpy(novo->nome, nome);
     strcpy(novo->descricao, descricao);
     novo->filho_esq = NULL;
     novo->filho_dir = NULL;
-    novo->pai = NULL;
-    novo->tem_decisao = tem_decisao;
-    novo->bloqueado = 0;
 
     return novo;
 }
@@ -27,7 +24,6 @@ void conectarMapas(NoMapa *pai, NoMapa *filho, int lado) {
     } else {
         printf("Numero invalido.");
     }
-    filho->pai = pai;
 }
 
 // Função para explorar o mapa
@@ -38,7 +34,7 @@ void explorar(NoMapa *local) {
 
     printf("\nVoce esta em: %s, %s\n", local->nome, local->descricao);
 
-    if (local->filho_esq || local->filho_dir || local->pai) {
+    if (local->filho_esq || local->filho_dir) {
         printf("Para onde deseja ir?\n");
 
         if (local->filho_esq) {
@@ -49,51 +45,18 @@ void explorar(NoMapa *local) {
             printf("2 - %s\n", local->filho_dir->nome);
         }
 
-        if (local->pai) {
-            printf("3 - Voltar para %s\n", local->pai->nome);
-        }
-
         int escolha;
         scanf("%d", &escolha);
 
-        if (escolha == 1 && local->filho_esq && local->filho_esq->bloqueado == 0) {
-            tomarDecisao(local, local->filho_esq);
-        } else if (escolha == 2 && local->filho_dir && local->filho_dir->bloqueado == 0) {
-            tomarDecisao(local, local->filho_dir);
-        } else if (escolha == 3 && local->pai && local->pai->bloqueado == 0) {
-            explorar(local->pai);
+        if (escolha == 1 && local->filho_esq) {
+            explorar(local->filho_esq);
+        } else if (escolha == 2 && local->filho_dir) {
+            explorar(local->filho_dir);
         } else {
-            printf("Opcao invalida ou mapa bloqueado! Tente novamente.\n");
+            printf("Opcao invalida! Tente novamente.\n");
             explorar(local);
         }
     } else {
         printf("Fim do caminho! Voce nao pode mais avancar.\n");
-    }
-}
-
-void tomarDecisao(NoMapa *origem, NoMapa *destino) {
-    if (origem->tem_decisao == 1 && origem->bloqueado == 0) {
-        printf("Voce esta prestes a tomar uma decisao importante!\n");
-        printf("Ir para %s bloqueara o caminho de volta para %s.\n", destino->nome, origem->nome);
-        printf("1. Sim, avancar.\n");
-        printf("2. Nao, ficar em %s.\n", origem->nome);
-
-        int escolha;
-        scanf("%d", &escolha);
-
-        if (escolha == 1) {
-            origem->bloqueado = 1;
-            printf("Voce avancou para %s. O caminho de volta para %s esta bloqueado!\n", 
-                   destino->nome, origem->nome);
-            explorar(destino);
-        } else if (escolha == 2) {
-            printf("Voce decidiu ficar em %s.\n", origem->nome);
-            explorar(origem);
-        } else {
-            printf("Escolha invalida! Tente novamente.\n");
-            tomarDecisao(origem, destino);
-        }
-    } else {
-        explorar(destino);
     }
 }
--- mapa.h
+++ mapa.h
@@ -6,15 +6,10 @@ typedef struct NoMapa {
     char descricao[100];
     struct NoMapa *filho_esq;
     struct NoMapa *filho_dir;
-    struct NoMapa *pai;
-    int tem_decisao;
-    int descricao_decisao;
-    int bloqueado;
 } NoMapa;
 
-NoMapa* criarNoMapa(char nome[], char descricao[], int tem_decisao);
+NoMapa* criarNoMapa(char nome[], char descricao[]);
 void conectarMapas(NoMapa *pai, NoMapa *filho, int lado);
 void explorar(NoMapa *local);
-void tomarDecisao(NoMapa *origem, NoMapa *destino);
 
 #endif
